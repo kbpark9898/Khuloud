0680ea14a84c2b21c1f7a67ea9d479a393513987
toror33 <toror33@naver.com>
2020-05-25 14:49:25 +0900

    회원정보 수정

--- backend/routes/userlogin/user.js
+++ backend/routes/userlogin/user.js
@@ -42,4 +42,39 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.post('/update', function(req, res, next) {
+  var user_id=req.body.user_id;
+  var user_pw=req.body.user_pw;
+  var user_pw2=req.body.user_pw2;
+  var user_email=req.body.user_email;
+  // var user_phone=req.body.user_phone;
+
+  if(user_pw===user_pw2) {
+    console.log(user_pw2);
+       // user_pw=CryptoJS.AES.encrypt(user_pw, 'secret key 123');
+	   	user_pw=cryptoM.encrypt(user_pw);
+        var sql='UPDATE users SET user_pw = ?, user_email= ? WHERE user_id = ?';
+        var values=[user_pw, user_email, user_id];
+        connection.query(sql, values , function (err) {
+          if (err) {
+            console.log("updating user failed");
+            //res.send({result:false});
+			  //throw err;
+          } else {
+            console.log("user updated successfully");
+			      res.redirect('back');
+          }
+        });
+  }
+  else
+  {
+    console.log("비밀번호가 일치하지 않습니다.");
+	res.send({result:false});
+    res.redirect('back');
+  }
+});
+
+
+
+
 module.exports = router;
