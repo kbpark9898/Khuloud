83ae25dcd6cb798dfeae92d37fe1cb6a3d33e033
박기범 <kbpark9889@khu.ac.kr>
2020-05-30 15:04:36 +0900

    '.'

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -35,6 +35,42 @@ function moveFolder(folderData) {
     return axios.post('/api/folder/move', folderData);
 }
 
+function file(curData){
+  return axios.get('/api/file', {
+      params: {
+          id: curData.id,
+          cur: curData.cur
+      }
+  });
+}
 
 
-export { registerUser, loginUser, folder, makeFolder, deleteFolder, moveFolder };
+function uploadFile(fileData){
+    return axios.post('/api/file/upload', fileData);
+}
+
+function deleteFile(fileData){
+    return axios.get('/api/file/delete', {
+      params:{
+        //현재 접속한 사람의 id와 삭제할 파일의 이름, 현재 폴더위치를 파라미터로 넘긴다.
+        id: fileData.id,
+        name: fileData.fileName,
+        cur: fileData.cur
+      }
+    });
+}
+function downloadFile(fileData){
+    return axios.get('api/file/download', {
+      params:{
+        //현재 접속한 사람의 id와 다운로드를 위해 선택한 파일의 이름을 파라미터로 넘긴다.
+        id: fileData.id,
+        name: fileData.fileName,
+        cur: fileData.cur
+      }
+    });
+}
+
+
+
+export { registerUser, loginUser, folder, makeFolder, deleteFolder, moveFolder,
+  file, uploadFile, deleteFile, downloadFile };
