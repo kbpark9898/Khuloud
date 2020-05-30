d60fe531aa9e57f652c8b5a3b470cc59157f470e
leeseohyeon <leeseohyeon21@gmail.com>
2020-05-30 16:47:14 +0900

    new branch

--- backend/routes/file/downloadFile.js
+++ backend/routes/file/downloadFile.js
@@ -8,22 +8,25 @@ AWS.config.loadFromPath(__dirname + "/../modules/awsconfig.json");
 var S3 = require('../modules/s3/s3');
 
 
+
 // /file/download/:name
 router.get('/:name', function (req, res) {
-    var file_name = req.params.name;
-    var user_id = req.session.user_id;
+    var file_name = req.params.name;    // test.txt
+    var user_id = req.query.id;
+    var curPath = req.query.cur;    // /folder1/folder2/
 
-    var sourceFile = file_name;
-    var tempDownloadDir = __dirname + '/../modules/s3/download/' + user_id + '/' + file_name;
+    var targetFile = (curPath + file_name).substring(1);  // folder1/folder2/test.txt
 
-    S3.downloadFile(S3.BUCKET_NAME, user_id, sourceFile, function (result, data) {
-        !fs.existsSync(tempDownloadDir + '/../') && fs.mkdirSync(tempDownloadDir + '/../');
-        fs.writeFileSync(tempDownloadDir, data);
-        res.download(tempDownloadDir, function (err) {
-            fs.unlink(tempDownloadDir, function (err) {
-                console.log("Download Success");
+    S3.downloadFile2(S3.BUCKET_NAME, user_id, targetFile, function (result, downloadDir) {
+        if (result){
+            res.download(downloadDir, function (err) {
+                fs.unlink(downloadDir, function (err) {
+                    console.log('download success');
+                });
             });
-        });
+        }else{
+            res.status(404).send({error: 'download error'});
+        }
     });
 });
 
--- backend/routes/file/files.js
+++ backend/routes/file/files.js
@@ -5,36 +5,46 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath(__dirname + "/../modules/awsconfig.json");
 var S3 = require('../modules/s3/s3');
 
+var fs = require('fs');
 
+
+// /file
 router.get('/', function (req, res) {
-    var user_id = req.session.user_id;
+    var user_id = req.query.id;
+    var curPath = req.query.cur;     // /folder1/folder2/
 
-    var sql = 'SELECT * FROM files WHERE user_id = (?) ORDER BY date DESC';
-    connection.query(sql, user_id, function (err, result) {
-        if (result.length == 0){
-            res.send({error: 'Exist Nothing'});
+    var sql = 'SELECT * FROM files WHERE user_id=(?) AND location=(?) ORDER BY date DESC';
+    connection.query(sql, [user_id, curPath], function (err, result) {
+        if (err){
+            res.send({error: 'select error'});
         }else{
+
             res.status(200).send({files: result});
         }
     })
 });
 
-// /file/:name
+
+
 router.get('/:name', function (req, res) {
     var file_name = req.params.name;
-    var user_id = req.session.user_id;
+    var curPath = req.query.cur;    // /folder1/folder2/
+    var user_id = req.query.id;
 
-    var s3 = new AWS.S3();
+    var targetFile = curPath.substring(1) + file_name;  // folder1/folder2/test.txt
+    var extension = targetFile.split('.')[1].toLowerCase();
 
-    var sourceFile = 'drive/' + user_id + '/' + file_name;
-    var params = { Bucket: S3.BUCKET_NAME, Key: sourceFile };
-    var stream = s3.getObject(params).createReadStream();
-    stream.pipe(res, function (err) {
-        if (err) {
-            throw err;
+
+    S3.downloadFile3(S3.BUCKET_NAME, user_id, targetFile, function(result, downloadDir){
+        if (result){
+            if (extension == 'jpg' || extension == 'jpeg' || extension == 'png') {
+                res.status(200).send({ type: 'image', src: downloadDir})
+            }else{
+                res.status(200).send({type: 'text', src: downloadDir})
+            }
         }
     })
-});
+}); 
 
 
 module.exports = router;
