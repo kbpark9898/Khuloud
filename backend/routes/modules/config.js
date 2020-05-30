6d47ceea94e0d23b68212991ea51debcbb8dc2ff
박기범 <kbpark9889@khu.ac.kr>
2020-05-30 15:07:57 +0900

    '.'

--- backend/routes/modules/config.js
+++ backend/routes/modules/config.js
@@ -1,10 +1,15 @@
 var mysql      = require('mysql');
 
 connection = mysql.createConnection({
-    host: '1.201.142.243',
-    user: 'khuloud',
+    //host: '1.201.142.243',
+    //user: 'khuloud',
+    //port: 3306,
+    //password: 'password1234',
+    //database: 'khuloud',
+    //connectionLimit: 30
+    host: 'localhost',
+    user: 'root',
     port: 3306,
-    password: 'password1234',
-    database: 'khuloud',
-    connectionLimit: 30
+    password: '1234',
+    database: 'temp'
   });
