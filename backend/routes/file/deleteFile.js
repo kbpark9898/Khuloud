4a9dd77a6bea5c86ff7beb1928bc9b3632fbd022
leeseohyeon <leeseohyeon21@gmail.com>
2020-05-31 16:59:15 +0900

    deleteFile 수정

--- backend/routes/file/deleteFile.js
+++ backend/routes/file/deleteFile.js
@@ -21,19 +21,19 @@ router.get('/:name', function (req, res) {
     connection.query(sql1, [file_name, curPath, user_id], function (err) {
         if (err) {
             console.log('delete db error');
-            res.status(404).send({erorr: 'db delete error'});
+            res.send({erorr: 'db delete error'});
         }
         else {
             connection.query(sql2, [file_name, '/trashcan'+curPath, user_id], function (err) {
                 if (err) {
                     console.log('insert in trashcan db error');
-                    res.status(404).send({error: 'db insert error'});
+                    res.send({error: 'db insert error'});
                 }
                 else {
                     // /drive/user_id/sourceFile --> /drive/user_id/trashcan/sourceFile
                     S3.moveFile2(S3.BUCKET_NAME, user_id, sourceFile, targetPath, function (result) {
                         if (result) {
-                            res.satus(200).send("move to trashcan success");
+                            res.send("move to trashcan success");
                         }
                     })
                 }
